f454e7c0872c5946b20e78fc5449d56fd989455d
ysknsid25 <kengo071225@gmail.com>
2023-04-20 21:05:12 +0900

    Constructorの動きの確認

---
+++ src/main/kotlin/constructor/Main.kt
@@ -0,0 +1,7 @@
+package constructor
+
+fun main(args: Array<String>) {
+    val person = Person("Alice", 29)
+    println(person.name)
+    println(person.age)
+}
---
+++ src/main/kotlin/constructor/Person.kt
@@ -0,0 +1,8 @@
+package constructor
+
+class Person(var name: String, val age: Int) {
+
+//    init {
+//        println("Person initialized $name")
+//    }
+}
